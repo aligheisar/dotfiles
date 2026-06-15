d3700ffd938cb849b42ae59f061758e53f31c3b4
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-15 15:13:18 +0330

    add fuzzel clipboard manager

--- .config/mango/scripts/restart-wlsunset.sh
+++ .config/mango/scripts/restart-wlsunset.sh
@@ -2,4 +2,4 @@
 
 ps aux | grep wlsunset | grep -v swayidle | grep -v restart | grep -v grep | awk '{print $2}' | xargs sudo kill 
 
-nohup wlsunset -l 32.4279 -L 53.6880 >> /dev/null 2<&1 &
+nohup wlsunset -l 32.4279 -L 53.6880 -t 5000 >> /dev/null 2<&1 &
