4bc751f9a589c5e14437d51b1fb1639367702a0d
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-18 19:21:11 +0330

    use hyprlock instead of swaylock in idle.

--- .config/mango/scripts/idle.sh
+++ .config/mango/scripts/idle.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 swayidle \
-	timeout 600 'wlr-dpms off && swaylock -f -c 000000' \
+	timeout 600 'wlr-dpms off && hyprlock' \
 	resume 'wlr-dpms on && ~/.config/mango/scripts/restart-wlsunset.sh' \
 	timeout 180 'dimland' \
 	resume '~/.config/mango/scripts/exit-dim.sh'
