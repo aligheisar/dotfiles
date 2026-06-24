94247999c7ef09a18e421cc205ba47b508ed82af
Ali Gheisar <ali.gheisar.dev@gmail.com>
2026-06-24 19:36:07 +0330

    lock system before sleep.

--- .config/mango/scripts/idle.sh
+++ .config/mango/scripts/idle.sh
@@ -4,4 +4,5 @@ swayidle \
 	timeout 600 'wlr-dpms off && hyprlock' \
 	resume 'wlr-dpms on && ~/.config/mango/scripts/restart-wlsunset.sh' \
 	timeout 180 'dimland' \
-	resume '~/.config/mango/scripts/exit-dim.sh'
+	resume '~/.config/mango/scripts/exit-dim.sh' \
+	before-sleep 'hyprlock' \
